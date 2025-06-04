f40c4bc304fd4760cd566b340e60ecb57c663e7a
itlavenkatasai <itlavenkatasai@gmail.com>
2025-06-04 23:39:00 +0530

    hr ques and answers

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+// function abc(){
+//     console.log(abc.xyz);
+// }
+// abc();
+// abc.xyz = 10;
+// abc.xyz = 20;
+// abc();
+// const numbers = [1,2,3,4];
+// numbers[100] = 500;
+// console.log(numbers);
+// console.log(typeof 100);
+
+// const arr = [...'sai'];
+// console.log(arr);
+// console.log(parseInt("2+2+3+#"));
+// console.log(parseInt("72FM"));
+// console.log(parseInt("M2F"))
+// console.log([1,2].map((num) => {
+//     if(num > 0) return;
+//     return num*2;
+// }))
 function abc(){
-    console.log();
+    console.log("hello");
+    
 }
 abc();
-abc.xyz = 10;
-abc.xyz = 20;
-abc();
+console.log(abc());
---
+++ inputfromusers.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+const start_num = prompt();
+const end_numm = prompt();
+console.log(start_num,end_numm);
+console.log(typeof(start_num),typeof(end_numm));
+console.log(parseInt(start_num),parseInt(end_numm));
